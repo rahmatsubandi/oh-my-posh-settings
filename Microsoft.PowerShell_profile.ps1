47183cce9a60ab2f84732189a715112e764d1aff
Rahmat Subandi <61180713+rahmatsubandi@users.noreply.github.com>
2022-12-25 02:48:35 +0700

    first
    PoweShell Profile (PC / Windows 11)
    oh-my-posh (My custom theme)

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,5 @@
+oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\sunflowx.omp.json | Invoke-Expression
+
+# Set-Alias neofetch winfetch
+# Remove-Item Alias:curl
+# clear
