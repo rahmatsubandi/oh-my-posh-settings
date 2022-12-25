4604a079dfc2f2bf9f6a2c5a2000b16bb5e4656e
Rahmat Subandi <61180713+rahmatsubandi@users.noreply.github.com>
2022-12-26 00:58:43 +0700

    Create (Win 10) Microsoft.PowerShell_profile.ps1

---
+++ (Win 10) Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Operator
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
