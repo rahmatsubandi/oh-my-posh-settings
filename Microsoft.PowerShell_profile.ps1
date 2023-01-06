83312c598c0ec8e128c873533051c968127a8220
Rahmat Subandi <61180713+rahmatsubandi@users.noreply.github.com>
2023-01-06 22:00:05 +0700

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,16 @@
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\sunflowx.omp.json | Invoke-Expression
 
 # LARAVEL ALIASES
-function artisan{php artisan $1}
 function dump-autoload{composer dump-autoload -o}
+function artisan{php artisan @args}
 function serve{php artisan serve}
 function optimize{php artisan optimize}
 function optimize:clear{php artisan optimize:clear}
 function migrate{php artisan migrate}
 function migrate:fresh{php artisan migrate:fresh --seed}
+function db{php artisan db:@args}
+function make{php artisan make:@args}
+function event{php artisan event:@args}
 
 # GIT ALIASES
 function status{git status}
