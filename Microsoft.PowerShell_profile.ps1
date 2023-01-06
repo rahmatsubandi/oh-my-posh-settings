703d3dc4e306d4d4f3b7463ef639d38d0a363027
Rahmat Subandi <61180713+rahmatsubandi@users.noreply.github.com>
2023-01-06 18:30:02 +0700

    add aliases
    - Laravel Commands
    - Git Commands

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,18 @@
 oh-my-posh --init --shell pwsh --config ~\AppData\Local\Programs\oh-my-posh\themes\sunflowx.omp.json | Invoke-Expression
 
-# Set-Alias neofetch winfetch
-# Remove-Item Alias:curl
-# clear
+# LARAVEL ALIASES
+function artisan{php artisan $1}
+function dump-autoload{composer dump-autoload -o}
+function serve{php artisan serve}
+function optimize{php artisan optimize}
+function optimize:clear{php artisan optimize:clear}
+function migrate{php artisan migrate}
+function migrate:fresh{php artisan migrate:fresh --seed --force}
+function migrate:refresh{php artisan migrate:refresh --seed --force}
+
+# GIT ALIASES
+function status{git status}
+function add{git add .}
+function commit{git commit -m @args}
+function push{git push}
+function pull{git pull}
