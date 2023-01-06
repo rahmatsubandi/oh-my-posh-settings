f269ead991521e76ca52c988100fa24b48bbf6ea
Rahmat Subandi <61180713+rahmatsubandi@users.noreply.github.com>
2023-01-06 23:22:54 +0700

    Delete (Win 10) Microsoft.PowerShell_profile.ps1

--- (Win 10) Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Operator
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
