54e6d77d94feba2de66659d0efc6173ef07d17eb
Rahmat Subandi <61180713+rahmatsubandi@users.noreply.github.com>
2023-01-19 12:58:03 +0700

    add GIT ALIASES
    - clone
    - branch
    - checkot

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -7,7 +7,7 @@ function serve{php artisan serve}
 function optimize{php artisan optimize}
 function optimize:clear{php artisan optimize:clear}
 function migrate{php artisan migrate}
-function migrate:fresh{php artisan migrate:fresh --seed}
+function migrate:fresh{php artisan migrate:fresh --seed --force}
 
 # GIT ALIASES
 function status{git status}
@@ -15,3 +15,6 @@ function add{git add .}
 function commit{git commit -m @args}
 function push{git push}
 function pull{git pull}
+function clone{git clone @args}
+function branch{git branch @args}
+function checkout{git checkout @args}
